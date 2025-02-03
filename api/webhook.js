1fd37491f43f5fdce7564538ca86dc390bb1bf54
Plrevolution <Evolutionplr@gmail.com>
2025-02-03 15:05:12 -0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,24 +1,20 @@
-const express = require('express');
-const app = express();
-const router = express.Router();
-
-app.use(express.json());
-
-router.post('/', (req, res) => {
-    const event = req.body.event;
-    switch (event) {
-        case 'event1':
-            // Lógica para tratar o evento 1
-            console.log('Evento 1 recebido');
-            break;
-        case 'event2':
-            // Lógica para tratar o evento 2
-            console.log('Evento 2 recebido');
-            break;
-        default:
-            console.log(`Evento não reconhecido: ${event}`);
+module.exports = (req, res) => {
+    if (req.method === 'POST') {
+        const event = req.body.event;
+        switch (event) {
+            case 'event1':
+                // Lógica para tratar o evento 1
+                console.log('Evento 1 recebido');
+                break;
+            case 'event2':
+                // Lógica para tratar o evento 2
+                console.log('Evento 2 recebido');
+                break;
+            default:
+                console.log(`Evento não reconhecido: ${event}`);
+        }
+        res.json({ received: true });
+    } else {
+        res.status(405).send('Método não permitido');
     }
-    res.json({ received: true });
-});
-
-module.exports = router;
+};
